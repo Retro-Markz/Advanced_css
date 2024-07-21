8188f11540c7cdb9dc0395d9d2b42ca7cc2323ba
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-07-21 08:29:08 -0600

    presentation animation Done

--- exercisse 1/app.js
+++ exercisse 1/app.js
@@ -25,5 +25,66 @@ $(document).ready(function () {
     $("#svg").append(newEllipse);
   });
 
-  $(window).mousemove(function (event) {});
+  $(window).mousemove(function (event) {
+    $("line").remove();
+    array
+      .filter(
+        (val) =>
+          Math.abs(val.top - event.pageY) <= 350 &&
+          Math.abs(val.left - event.pageX) <= 350
+      )
+      .forEach(function (value) {
+        var newLine = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "line"
+        );
+        newLine.setAttribute("class", "line");
+        newLine.setAttribute("id", "line_" + value.id);
+        newLine.setAttribute("x1", value.left);
+        newLine.setAttribute("y1", value.top);
+        newLine.setAttribute("x2", event.pageX);
+        newLine.setAttribute("y2", event.pageY);
+        newLine.setAttribute("stroke", "black");
+        $("#svg").append(newLine);
+      });
+  });
+  $(window).mouseout(function (event) {
+    $("line").remove();
+  });
+  setInterval(function () {
+    // Math.random()*(max - min ) + min
+    array.forEach(function (value, key) {
+      var top =
+        Math.random() * (value.top + 2 - (value.top - 2)) + (value.top - 2);
+      var left =
+        Math.random() * (value.left + 2 - (value.left - 2)) + (value.left - 2);
+      array[key].top = top;
+      array[key].left = left;
+
+      var $ellipse = $("#ellipse_" + value.id);
+      $ellipse.animate(
+        { dot: 0 },
+        {
+          step: () => {
+            $ellipse.attr("cx", left), $ellipse.attr("cy", top);
+          },
+          duration: 1,
+        }
+      );
+
+      if ($("#line_" + value.id).lengh) {
+        var $line = $("#line_" + value.id);
+        $line.animate(
+          { dot: 0 },
+          {
+            step: () => {
+              $line.attr("x1", left), $line.attr("y1", top);
+            },
+            duration: 1,
+          }
+        );
+      }
+    });
+  }, 500);
+  console.log(array);
 });
