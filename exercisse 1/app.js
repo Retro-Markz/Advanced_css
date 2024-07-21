5d369d2e247a9fad78623379ae0bb77ebf884f02
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-07-21 07:19:37 -0600

    update

--- exercisse 1/app.js
+++ exercisse 1/app.js
@@ -1,13 +1,29 @@
-$(document).ready(funciton(){
-    var array = [];
-    var heightWindow = $(window).heigh();
-    var widthWindow = $(window).width();
+$(document).ready(function () {
+  var array = [];
+  var heightWindow = $(window).height();
+  var widthWindow = $(window).width();
 
-    for(var i = 0; i< 40; i++){
+  for (var i = 0; i < 40; i++) {
     array.push({
-        top: Math.random()*heightWindow,
-        left: Math.random()*widthWindow,
-        id: i
-    })  
-    }
-})
+      top: Math.floor(Math.random() * heightWindow),
+      left: Math.floor(Math.random() * widthWindow),
+      id: i,
+    });
+  }
+  array.forEach(function (value) {
+    var newEllipse = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "ellipse"
+    );
+    newEllipse.setAttribute("class", "ellipse");
+    newEllipse.setAttribute("id", "ellipse_" + value.id);
+    newEllipse.setAttribute("cx", value.left);
+    newEllipse.setAttribute("cy", value.top);
+    newEllipse.setAttribute("rx", 5);
+    newEllipse.setAttribute("ry", 5);
+
+    $("#svg").append(newEllipse);
+  });
+
+  $(window).mousemove(function (event) {});
+});
